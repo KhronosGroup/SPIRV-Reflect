e8f007927e9222e873bcc28f689ae68708d0e2a0
Spencer Fricke <115671160+spencer-lunarg@users.noreply.github.com>
2025-01-15 15:22:04 -0500

    Add SpvReflectVariableFlags to YAML (#298)

--- common/output_stream.h
+++ common/output_stream.h
@@ -19,6 +19,7 @@ std::string ToStringShaderStage(SpvReflectShaderStageFlagBits stage);
 std::string ToStringResourceType(SpvReflectResourceType type);
 std::string ToStringDescriptorType(SpvReflectDescriptorType value);
 std::string ToStringTypeFlags(SpvReflectTypeFlags type_flags);
+std::string ToStringVariableFlags(SpvReflectVariableFlags flags);
 std::string ToStringDecorationFlags(SpvReflectDecorationFlags decoration_flags);
 std::string ToStringDescriptorType(SpvReflectDescriptorType value);
 std::string ToStringFormat(SpvReflectFormat fmt);
